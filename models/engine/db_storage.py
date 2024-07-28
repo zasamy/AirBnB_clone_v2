6dd0255ac296a021f3ba435d66e75e7d80451fd9
zasamy <zainebzahiri@gmail.com>
2024-07-28 14:25:39 -0700

    air bnb clone

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,4 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,4 +68,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
